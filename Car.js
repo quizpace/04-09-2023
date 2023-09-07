4a59bfe4a26f2967b9b66ae04fcc17bdfac75acc
Avi <drazninavi@gmail.com>
2023-09-07 17:16:56 +0300

    up

---
+++ Car.js
@@ -0,0 +1,11 @@
+class Car {
+  constructor(brand, model, price, license, year, color, maintenanceYears) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+    this.license = license;
+    this.year = year;
+    this.color = color;
+    this.maintenanceYears = maintenanceYears;
+  }
+}
---
+++ Phone.js
@@ -0,0 +1,10 @@
+class Phone {
+  constructor(brand, model, color, storageCapacity, batteryLife, poweredOn) {
+    this.brand = brand;
+    this.model = model;
+    this.color = color;
+    this.storageCapacity = storageCapacity;
+    this.batteryLife = batteryLife;
+    this.poweredOn = poweredOn;
+  }
+}
